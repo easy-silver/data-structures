f6fcde2d7c82271a2c9698524a51f01b8bd8aac5
Timo <developer.timo@gmail.com>
2022-11-27 19:54:26 +0900

    [6주차 - 해시테이블] 해시테이블 자료구조 구현
    - division 방식으로 해시 함수를 구현

--- src/main/kotlin/study/dinosaur/datastructures/w6_hashtable/Hashtable.kt
+++ src/main/kotlin/study/dinosaur/datastructures/w6_hashtable/Hashtable.kt
@@ -1,5 +1,28 @@
 package study.dinosaur.datastructures.w6_hashtable
 
 class Hashtable {
+    private val table = Array(100) { mutableListOf<String>() }
 
+    fun add(value: String) {
+        val index = hash(value)
+        table[index].add(value)
+    }
+
+    fun remove(value: String) {
+        val index = hash(value)
+        table[index].remove(value)
+    }
+
+    fun contains(value: String): Boolean {
+        val index = hash(value)
+        return table[index].contains(value)
+    }
+
+    private fun hash(value: String): Int {
+        var hash = 0
+        for (i in value.indices) {
+            hash += value[i].code * i
+        }
+        return hash % table.size
+    }
 }
